2b630a719bdeca0768a719fcb7b3aaf5facefa36
Aelnonym <aelnonym@gmail.com>
2022-09-09 19:09:57 -0300

    Versao 3

--- Aula_2/Program.cs
+++ Aula_2/Program.cs
@@ -5,10 +5,10 @@ namespace aula_02
     {
         //Classes -------------------------------------------------------
 
-        public class Criacao_Mapa{
+        public class Memes_Brasil{
             public string[,] mapa = new string[10,10]; //Criando o mapa
 
-            public Criacao_Mapa(){
+            public Memes_Brasil(){
                 for(int i = 0; i < 10; i++){
                     for(int j = 0; j < 10; j++){
                         mapa[i,j] = "--";
@@ -17,21 +17,21 @@ namespace aula_02
             }
         }
     
-    public static void printa_tudo(string coisa){
+    public static void printa_tudo(string[,] pure_de_batatas){
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
-                Console.WriteLine(coisa[i,j]);
+                Console.WriteLine(pure_de_batatas[i,j]);
             }
         }
     }
         //Chamando tudo -------------------------------------------------------
     public static void Main()
         {
-            var jogo = new Criacao_Mapa();
+            var jogo = new Memes_Brasil();
                           
             printa_tudo(jogo.mapa);
 
         }  
     }      
 }
-// Versao 2 Main
+// Versao 3 Main
